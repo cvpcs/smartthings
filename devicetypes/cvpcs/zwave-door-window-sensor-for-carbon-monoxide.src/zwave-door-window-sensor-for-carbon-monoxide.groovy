4eb3c0bf8e607c6e187a982760656e7f74c4063a
Austen Dicken <cvpcsm@gmail.com>
2019-03-09 14:03:29 -0600

    Remove unknown data to see if publishing will work again

--- devicetypes/cvpcs/zwave-door-window-sensor-for-carbon-monoxide.src/zwave-door-window-sensor-for-carbon-monoxide.groovy
+++ devicetypes/cvpcs/zwave-door-window-sensor-for-carbon-monoxide.src/zwave-door-window-sensor-for-carbon-monoxide.groovy
@@ -14,7 +14,7 @@
  */
 
 metadata {
-	definition (name: "Z-Wave Door/Window Sensor for Carbon Monoxide Relay", namespace: "cvpcs", author: "Austen Dicken", genericHandler: "Z-Wave", mnmn: "SmartThings", vid: "generic-smoke") {
+	definition (name: "Z-Wave Door/Window Sensor for Carbon Monoxide Relay", namespace: "cvpcs", author: "Austen Dicken", genericHandler: "Z-Wave") {
 		capability "Smoke Detector"
 		capability "Carbon Monoxide Detector"
 		capability "Sensor"
